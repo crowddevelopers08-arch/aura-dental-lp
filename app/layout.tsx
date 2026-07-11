9bc3fb728faf8139c7f4db95a39816ebc7bde2a0
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-11 13:50:22 +0530

    tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,10 +32,33 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T7RCDSHC');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${ebGaramond.variable} ${openSans.variable} font-body selection:bg-[#1D4231] selection:text-[#D3BB71]`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T7RCDSHC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
